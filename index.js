02245659a8b46082c121f0188e4b69a3ee32e8a7
Àlex Serra <bounteous17@protonmail.com>
2024-05-09 14:30:27 +0200

    optional psql capabilities

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 // Import required modules
 const express = require("express");
+const pg = require("pg");
 
 // Create an instance of Express
 const app = express();
 
+// Create an instance of PG client
+const connectPSQL = process.env.PGCONNECT === "true";
+let pgClient;
+
+if (connectPSQL) {
+  console.info("Trying to connect PSQL server ...");
+  pgClient = new pg.Client();
+  pgClient.connect().catch((error) => {
+    console.error(error);
+  });
+} else {
+  console.info("Ignoring PSQL server connection attempt");
+}
+
 // Define a route for GET requests to the root URL '/'
 app.get("/", (req, res) => {
   res.send("Hello, World!");
+});
+
+app.get("/psql", async (req, res) => {
+  res.json({ connected: pgClient._connected });
 });
 
 // Start the server on port 3000
